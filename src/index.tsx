f2606580657f83a8e2d7ce83e529e290bbb3235c
yi-lin-1234 <92185695+yi-lin-1234@users.noreply.github.com>
2023-08-02 22:21:00 -0400

    fix deploy

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = process.env.REACT_APP_DOMAIN!;
-const clientId = process.env.REACT_APP_CLIENT_ID!;
+// const domain = process.env.REACT_APP_DOMAIN!;
+// const clientId = process.env.REACT_APP_CLIENT_ID!;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
+      domain="dev-wbntuus1r5ywt5kh.us.auth0.com"
+      clientId="7nMtno8Oqk9We5DiegxC8Jhv1xr61E88"
       authorizationParams={{
         redirect_uri: "https://inspiring-squirrel-e515f6.netlify.app/dashboard",
         audience: "8707129838",
